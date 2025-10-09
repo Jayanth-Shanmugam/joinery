d1ab17276659d3c7e201be92fd137370265a130e
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2025-10-09 00:46:27 -0400

    Add split query to parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,1 +1,18 @@
+from typing import List
+from sqlgot import parse_one, exp
 
+def _split_query(query: str) -> List[str]:
+  '''
+  Split a federated query across different databases into individual queries
+  for each database
+
+  Parameters
+    query -> A query string that references multiple databases
+  Returns
+    List of individual queries for each database
+  '''
+
+  if not string:
+    raise ValueError("Empty SQL query!")
+
+  
