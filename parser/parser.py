b912120211b96e4937c09cdd4382a12c2be414bb
Jayanth Shanmugam <jayanth.mks@gmail.com>
2025-10-23 22:15:55 -0400

    Add split query functionality
    Added functionality to split query into tables and columns

--- parser/parser.py
+++ parser/parser.py
@@ -1,18 +1,36 @@
-from typing import List
-from sqlgot import parse_one, exp
+import sqlglot.errors
+from sqlglot import parse_one, exp, transpile
 
-def _split_query(query: str) -> List[str]:
-  '''
-  Split a federated query across different databases into individual queries
-  for each database
 
-  Parameters
-    query -> A query string that references multiple databases
-  Returns
-    List of individual queries for each database
-  '''
+def _split_query(query: str) -> list[str]:
+    """
+    Split a federated query across different databases into individual queries
+    for each database
 
-  if not string:
-    raise ValueError("Empty SQL query!")
+    Parameters
+      query -> A query string that references multiple databases
+    Returns
+      List of individual queries for each database
+    """
 
-  
+    try:
+        _ = transpile(query)
+    except sqlglot.errors.ParseError as e:
+        print(e.errors)
+
+    # Create a list of tables being referenced in the query
+    tables: list[str] = []
+    for table in parse_one(query).find_all(exp.Table):
+        tables.append(table.alias)
+
+    # Create a list of columns being referenced in the query
+    columns: list[str] = []
+    for column in parse_one(query).find_all(exp.Column):
+        columns.append(column.alias_or_name)
+
+    return tables
+
+
+if __name__ == "__main__":
+    query = "SELECT * FROM department a JOIN employees b ON a.id = b.dept_id"
+    print(_split_query(query))
